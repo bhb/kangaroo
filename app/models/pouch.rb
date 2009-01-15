bac92dc0c76218effabfe46a3fd34caeff3baad9
Kurt Schrader <kurt@karmalab.org>
2009-01-15 00:44:41 -0800

    Make sure that we add in a proper link for downloading a zip of the files in a pouch.

--- app/models/pouch.rb
+++ app/models/pouch.rb
@@ -37,7 +37,7 @@ class Pouch
     # open or create the zip file
     Zip::ZipFile.open(bundle_filename, Zip::ZipFile::CREATE) {
       |zipfile|
-      zipfile.add("README","#{Merb.root}/README")
+      zipfile.add("README","#{Merb.root}/README.rdoc")
       # collect the files
       self.uploads.collect {
         |file|
