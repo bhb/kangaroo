12ff9d51ba5c8a20199b1a84396f9c7f3d336901
Kurt Schrader <kurt@karmalab.org>
2009-01-14 17:01:08 -0800

    Add in the mime_type stuff for zipped files.

--- config/init.rb
+++ config/init.rb
@@ -14,6 +14,8 @@ Merb::Config.use do |c|
   c[:session_secret_key]  = '095200501e1a82ece68f0ffbe55fad37b250803a'  # required for cookie session store
   c[:session_id_key] = '_kangaroo_session_id' # cookie session id key, defaults to "_session_id"
 end
+
+Merb.add_mime_type(:zip, :to_zip, %w[application/zip])
  
 Merb::BootLoader.before_app_loads do
   # This will get executed after dependencies have been loaded but before your app's classes have loaded.
